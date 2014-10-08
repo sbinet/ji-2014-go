40a5e636575275d486a76be14a86137da5bed028
Sebastien Binet <binet@cern.ch>
2014-10-08 14:12:14 +0200

    all: remove ex-03 (put in extra-01). add map-reduce example from talks.golang.org/2014/taste

--- slides/_code/ex-03.go
+++ slides/_code/ex-03.go
@@ -1,35 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
+	"io"
 	"os"
-	"strings"
+
+	"github.com/ji-go/rot13" // HLxxx
 )
 
 func main() {
-	fname := os.Args[1]
-	f, err := os.Open(fname) // HLxxx
-	if err != nil {
-		log.Fatalf("could not open %q: %v\n", fname, err)
-	}
-	defer f.Close() // HLxxx
-
-	words := 0
-	scan := bufio.NewScanner(f)
-	for scan.Scan() {
-		line := strings.Split(scan.Text(), " ")
-		for _, word := range line {
-			if word != "" {
-				words++
-			}
-		}
-	}
-
-	err = scan.Err() // HLxxx
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "**error: %v\n", err)
-	}
-	fmt.Printf("%d\n", words)
+	r := rot13.NewReader(os.Stdin)
+	io.Copy(os.Stdout, r)
 }
